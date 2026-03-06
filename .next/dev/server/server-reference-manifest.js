52f212ea72e0a4cc8c2909c68f30b60cc02b35a2
Ashish Kumar <15678ashishk@gmail.com>
2026-03-06 13:09:39 +0530

    remove .next
    had accidentally added it , idk when

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"e9HruGHJn0WJsbbH9kK3cKyrs/9YoWlt9NKjBDxqLCc=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__cc58851c._.css",
-  "static/chunks/_ed2ecb00._.js"
-],
-    source: "dynamic"
-});
