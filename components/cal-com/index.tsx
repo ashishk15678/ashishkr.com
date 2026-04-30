646d81c9676148315280db2e046aea727659b0ae
Ashish Kumar <15678ashishk@gmail.com>
2026-04-30 21:41:23 +0530

    change from cal to calendly dot com

--- components/cal-com/index.tsx
+++ components/cal-com/index.tsx
@@ -1,18 +1,32 @@
 "use client";
-import { getCalApi } from "@calcom/embed-react";
-import { useEffect } from "react";
+import { InlineWidget } from "react-calendly";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 export default function CalComponent() {
-  useEffect(() => {
-    (async function () {
-      const cal = await getCalApi({ namespace: "30min" });
-      cal("floatingButton", {
-        calLink: "ashish15678/30min",
-        config: { layout: "week_view", useSlotsViewOnSmallScreen: "true" },
-        buttonColor: "#3de600",
-        buttonTextColor: "#000000",
-      });
-      cal("ui", { hideEventTypeDetails: false, layout: "week_view" });
-    })();
-  }, []);
-  return <></>;
+  const router = useRouter();
+  return (
+    <>
+      <div className="w-full h-full">
+        <div className="">
+          <Button
+            onClick={() => {
+              router.push("/#calendar-booking");
+            }}
+            className="fixed bottom-2 right-2 bg-primary shadow-sm shadow-primary p-4 px-6 rounded-md text-secondary text-lg"
+          >
+            Book calendar
+          </Button>
+        </div>
+        <section id="calendar-booking" data-scroll-behavior="smooth">
+          <InlineWidget
+            id="calendar-booking"
+            url="https://calendly.com/15678ashish/book"
+            styles={{
+              height: "700px",
+            }}
+          />
+        </section>
+      </div>
+    </>
+  );
 }
